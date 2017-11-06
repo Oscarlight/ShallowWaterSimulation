26bd29b0271d41f076e23376bdcc6f2bd0565fcc
OscarLight <oscarli2716@gmail.com>
2017-11-06 14:22:29 -0500

    commit

--- src-cuda/stepper_test.cu
+++ src-cuda/stepper_test.cu
@@ -72,6 +72,7 @@ int main(int argc, char** argv){
     cudaMalloc( (void**)&dev_g, N );
     cudaMalloc( (void**)&dev_scratch, 6*nx_all*sizeof(float) );
 
+    cudaMemcpy( dev_u, u, N, cudaMemcpyHostToDevice);
     cudaMemcpy( dev_v, v, N, cudaMemcpyHostToDevice);
     cudaMemcpy( dev_f, f, N, cudaMemcpyHostToDevice);
     cudaMemcpy( dev_g, g, N, cudaMemcpyHostToDevice);
