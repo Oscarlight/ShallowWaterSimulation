ce46fb4ca83465c28b456ca5671568e721523921
OscarLight <oscarli2716@gmail.com>
2017-11-06 17:12:55 -0500

    commit

--- src-cuda/stepper_base.c
+++ src-cuda/stepper_base.c
@@ -246,8 +246,8 @@ void central2d_predict_base_linear(
           //   k, ix, iy, offset_ix, u[offset_ix]);
           // printf(">>> (k, ix, iy): %d, %d, %d \t (u[offset_ix], fx[ix], gy[ix]) %f, %f, %f \n", 
           //   k, ix, iy, u[offset_ix], fx[ix], gy[ix]);
-          printf(">>> (k, ix, iy): %d, %d, %d \t %f, %d\n", 
-            k, ix, iy, v[offset_ix], offset_ix);
+          // printf(">>> (k, ix, iy): %d, %d, %d \t %f, %d\n", 
+          //   k, ix, iy, v[offset_ix], offset_ix);
           // printf(">>> (k, ix, iy): %d, %d, %d \t %f, %f, %f, -> %f\n", 
           //   k, ix, iy, f[ix-1+offset], f[ix+offset], f[ix+1+offset], fx[ix]);
         }
