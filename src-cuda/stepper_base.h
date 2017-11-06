335163230b8da76ac638e07953f8487dc1361b83
OscarLight <oscarli2716@gmail.com>
2017-11-06 00:15:52 -0500

    commit

--- src-cuda/stepper_base.h
+++ src-cuda/stepper_base.h
@@ -3,13 +3,15 @@
 
 #include <math.h>
 
-void central2d_predict(float* restrict v,
-                       float* restrict scratch,
-                       const float* restrict u,
-                       const float* restrict f,
-                       const float* restrict g,
-                       float dtcdx2, float dtcdy2,
-                       int nx, int ny, int nfield);
+void central2d_predict(
+    float* restrict v, 
+    float* restrict scratch,
+    const float* restrict u,
+    const float* restrict f,
+    const float* restrict g,
+    float dtcdx2, float dtcdy2,
+    int nx, int ny, int nfield
+);
 
 void central2d_correct(float* restrict v,
                        float* restrict scratch,
